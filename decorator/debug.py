c9d9cdf8f5f0d8082d9665ed7198eb7763160bc2
Ted <ski2per@163.com>
2017-05-11 11:53:49 +0800

    Update debug.py

--- decorator/debug.py
+++ decorator/debug.py
@@ -1,24 +1,18 @@
-def connect2redis(host="localhost", port=6379, db=0):
-    def wrapper1(f):
-        def wrapper2(*args, **kwargs):
-            redis_conn = "{}:{}  {}".format(host, port, db)
-            f(*args, **kwargs)
-        return wrapper2
-    return wrapper1
-
-def ver(v):
-    # outer wrapper with 'f(target function)' as argument
-    def wrapper(f):
-        # inner wrapper with '*args' as arguments for target function
-        def wrapper_f(*args):
-            return "[{}] {}".format(v, f(*args))
-        return wrapper_f
-    return wrapper
+def setup(host="localhost", port=6379, db=0):
+    def wrapped(func):
+        def new_func(*args, **kwargs):
+            conn = "{}:{} - {}".format(host, port, db)
+            return func(conn, *args, **kwargs)
+        return new_func
+    return wrapped
 
 
-@connect2redis("127.0.0.1", 8888, 0)
-def say_hello(name):
-    return "hello {}".format(name)
+@setup("127.0.0.1", 8888, 4)
+# @setup()
+def do_sth(conn):
+#def do_sth(conn, name):
+    print(conn)
+    # print(name)
 
-
-print(say_hello("Ted"))
+# do_sth("Ted")
+do_sth()
