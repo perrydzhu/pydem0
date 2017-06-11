6590c506d9780c72ebda8a868895dbed2c9e4f97
ski2per <ski2per@163.com>
2017-06-11 22:46:34 +0800

    update

--- flask/test.py
+++ flask/test.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python
-
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Hello Skipper"
+    return "Hello !"
+
+
+@app.route("/data")
+def return_data():
+    data = {
+        "2011": 3,
+        "2012": 18,
+        "2013": 5
+    }
+
+    return jsonify(data)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000)
-    #app.run(host="0.0.0.0",port=8000,threaded=True)
-
+    app.run(host="0.0.0.0", port=8000)
