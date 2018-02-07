e9e3367214c7f628b9c753853e115fc03a8d691b
Ted <ski2per@163.com>
2018-02-07 23:23:49 +0800

    add bubble sort

--- algorithm/sorting/bubble_sort.py
+++ algorithm/sorting/bubble_sort.py
@@ -1,1 +1,17 @@
+def bubble_sort(unsorted):
+    n = len(unsorted)
+    for i in range(1, n):
+        # print(range(n-i-1))
+        for j in range(n-i):
+            if unsorted[j] > unsorted[j+1]:
+                tmp = unsorted[j]
+                unsorted[j] = unsorted[j+1]
+                unsorted[j+1] = tmp
+        print(unsorted)
 
+    return unsorted
+
+
+if __name__ == '__main__':
+    numbers = [2, 7, 4, 1, 5, 3]
+    print(bubble_sort(numbers))
