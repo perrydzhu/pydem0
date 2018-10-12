379b329cb14a0e30036f5b5cb69694e9ac7a0cfb
zhangpeng <ski2per@163.com>
2018-10-12 17:43:10 +0800

    update

--- ssh/sshexec.py
+++ ssh/sshexec.py
@@ -2,9 +2,14 @@ import sys
 import paramiko
 
 
-if len(sys.argv) != 5:
-    print("Usage: python {0} host username password command".format(sys.argv[0]))
+param = len(sys.argv)
+background = False
+
+if param < 5:
+    print("Usage: python {0} host username password command <bg>".format(sys.argv[0]))
     sys.exit(-1)
+if param == 6:
+    background = True
 
 host = sys.argv[1]
 username = sys.argv[2]
@@ -15,9 +20,15 @@ try:
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=host, port=22, username=username, password=password)
-    stdin, stdout, stderr = client.exec_command(command)
-    print(stdout.read())
-    print(stderr.read())
+    if background:
+        transport = client.get_transport()
+        channel = transport.open_session()
+        channel.exec_command(command)
+        print("Run command '{0}' in background on remote host {1}".format(command, host))
+    else:
+        stdin, stdout, stderr = client.exec_command(command)
+        print(stdout.read())
+        print(stderr.read())
 except Exception as e:
     print(e)
     print("Error happened when invoking script on {0}".format(host))
